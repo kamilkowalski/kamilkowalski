532bf66b4de879064f9f4e307274890f62e431ce
Kamil Kowalski <kowalski.tychy@gmail.com>
2012-09-05 12:12:11 +0200

    Setting up Rails with standard gems and CSS libraries

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.5'
+
+group :production do
+	gem 'pg'
+end
+
+group :development, :test do
+	gem 'sqlite3'
+	gem 'debugger'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'compass-rails'
+  gem 'compass-normalize'
+end
+
+gem 'jquery-rails'
+gem 'haml-rails'
+gem 'kaminari'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kamilkowalski::Application.config.secret_token = 'cbfb3abc5a28114f29f7b3c9fe74d045bb020e5d77ee4488823204af8d34b36a687f2b0337ebccac8161543aa921b59ae2b42c63e7dc7f2b9f245297f94dc1aa'
